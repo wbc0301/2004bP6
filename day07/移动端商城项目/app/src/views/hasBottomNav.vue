c67ec068e271b33cc9762235f62a13cb098483f1
wbc <417780894@qq.com>
2020-12-27 20:09:02 +0800

    动画

--- day07/移动端商城项目/app/src/views/hasBottomNav.vue
+++ day07/移动端商城项目/app/src/views/hasBottomNav.vue
@@ -1,6 +1,9 @@
 <template>
-	<div class='hasBottomNav'>
-		<router-view></router-view>
+	<div class="hasBottomNav">
+		<transition>
+			<!-- <div v-if="aa">dasdfasdfasdfasdfasdfasdfasdfasdfsdfasd</div> -->
+			<router-view></router-view>
+		</transition>
 
 		<van-tabbar v-model="active">
 			<van-tabbar-item icon="home-o" to="/home">首页</van-tabbar-item>
@@ -16,14 +19,41 @@ export default {
 	data() {
 		return {
 			active: 0,
+			aa: 1,
 		};
 	},
-	mounted() {},
+	mounted() { },
 	methods: {},
 };
 </script>
 
 <style lang='scss' scoped>
 .hasBottomNav {
+	.v-enter { // 在元素被插入之前生效，在元素被插入之后的下一帧移除。
+		transform: translatex(100%);
+	}
+	.v-enter-active { // 在元素被插入之前生效，在过渡/动画完成之后移除
+		transition: all 0.3s;
+		position: absolute;
+		top: 0px; 
+		width: 100%;
+	}
+	.v-enter-to { // 在元素被插入之后下一帧生效 (与此同时 v-enter 被移除)，在过渡/动画完成之后移除。
+		transform: translatex(0);
+	}
+
+	.v-leave { // 在离开过渡被触发时立刻生效，下一帧被移除
+		transform: translatex(0);
+	}
+
+	.v-leave-active { // 在离开过渡被触发时立刻生效，在过渡/动画完成之后移除
+		transition: all 0.3s;
+		position: absolute;
+		top: 0px;
+		width: 100%;
+	}
+	.v-leave-to { // 在离开过渡被触发之后下一帧生效 (与此同时 v-leave 被删除)，在过渡/动画完成之后移除
+		transform: translatex(-100%);
+	}
 }
 </style>
