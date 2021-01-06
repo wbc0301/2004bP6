11caf075f9a0489d836e0d0cd1a07b126fdd5366
wbc <417780894@qq.com>
2021-01-06 10:12:33 +0800

    moment

---
+++ day18/moment-demo/app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="app">
+        购买日期：{{time}}
+        <div>{{$moment(time).format('YYYY/MM/DD HH:mm:ss')}}</div>
+        <hr>
+        <div>{{$moment(time).format('YYYY-MM-DD HH:mm:ss')}}</div>
+        <hr>
+        <div>{{$moment(time).format('YYYY-MM-DD HH:mm')}}</div>
+        <hr>
+        <div>{{$moment(time).format('YYYY-MM-DD')}}</div>
+        <hr>
+        <div>{{$moment(time).format('YYYY年MM月DD')}}</div>
+        <hr>
+        <div>{{$moment(time).format('YYYY')}}</div>
+        <hr>
+        <div>{{$moment(time).format('HH:mm:ss')}}</div>
+        <hr>
+        <div>{{$moment(time).format('YYYY年M月DD')}}</div>
+        <hr>
+        <div>{{$moment(time).format('YYYY年M月DD 星期d')}}</div>
+        <hr>
+	</div>
+</template>
+
+<script>
+// moment官网： http://momentjs.cn/docs/#/displaying/format/
+export default {
+    data() {
+        return {
+            time: +new Date() - 2 * 60 * 60 * 1000,
+        }
+    },
+    mounted() {
+        this.$moment().format('')
+    }
+}
+</script>
+
---
+++ day18/moment-demo/app/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+import moment from 'moment';
+Vue.prototype.$moment = moment;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
